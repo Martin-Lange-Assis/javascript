49869ddbe6067353689ee23b36904d90b3db74a4
Martin Lange de Assis <101527940+Martin-Lange-Assis@users.noreply.github.com>
2023-10-10 08:54:26 -0300

    Update Ex002.js
    Documentação do código JS

--- praticandoJS/exe002/Ex002.js
+++ praticandoJS/exe002/Ex002.js
@@ -13,24 +13,36 @@ Exibir o resultado: Mostre ao usuário o valor da gorjeta calculada. Isso pode s
 
 Finalizar: Depois de calcular e exibir o valor da gorjeta, seu aplicativo estará pronto para uso. O usuário poderá usar sua calculadora de gorjetas para obter um cálculo rápido e preciso das gorjetas a serem deixadas no restaurante.
 */
+
+// Aguarda o carregamento completo do documento antes de executar o código
 document.addEventListener("DOMContentLoaded", function () {
+    // Adiciona um ouvinte de evento ao botão com o ID "gorjeta" e define a função "calcularGorjeta" como a ação ao clicar
     document.getElementById("gorjeta").addEventListener("click", calcularGorjeta);
 
+    // Variável para armazenar o valor da gorjeta
     var gorjeta;
 
+    // Função para calcular a gorjeta
     function calcularGorjeta() {
+        // Obtém o valor total da conta inserido pelo usuário e converte para um número de ponto flutuante
         var valorTotConta = parseFloat(document.getElementById("inputValorTotConta").value);
+        
+        // Obtém o valor da porcentagem da gorjeta inserido pelo usuário e converte para um número de ponto flutuante
         var porcGorjeta = parseFloat(document.getElementById("inputPorcentagem").value);
+
+        // Calcula a gorjeta com base no valor total da conta e na porcentagem da gorjeta
         gorjeta = (valorTotConta * porcGorjeta) / 100;
+
+        // Verifica se o valor total da conta é inválido ou não foi informado
         if (isNaN(valorTotConta) || valorTotConta <= 0) {
             window.alert("Informe um valor a ser convertido");
-        } else if (isNaN(porcGorjeta) || porcGorjeta <= 0) {
+        }
+        // Verifica se o valor da porcentagem da gorjeta é inválido ou não foi informado
+        else if (isNaN(porcGorjeta) || porcGorjeta <= 0) {
             window.alert("Informe a porcentagem que será recebida");
         } else {
+            // Exibe o resultado da gorjeta no elemento HTML com o ID "resultado"
             document.getElementById("resultado").innerHTML = `Você recebeu uma gorjeta de R$ ${gorjeta}`;
         }
-
-
     }
-
-})
+});
